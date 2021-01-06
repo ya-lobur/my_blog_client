ca906818bf0de6f91bb86d45389826d10c893e39
Лобурь Ярослав <lobur@rasse.ru>
2021-01-06 19:45:20 +0300

    * добавляет конфиг для antd * добавляет moment и локаль для него

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import { Provider } from 'react-redux'
 import store from './redux/store';
 import './App.css';
 import AppRouter from 'layouts/AppRouter';
+import { ConfigProvider } from 'antd';
+import locale from 'antd/es/locale/ru_RU';
+import moment from 'moment';
+import 'moment/locale/ru';
+
+moment.locale('ru');
 
 
 function App() {
     return (
         <Provider store={store}>
-            <AppRouter />
+            <ConfigProvider locale={locale}>
+                <AppRouter/>
+            </ConfigProvider>
         </Provider>
     );
 }
