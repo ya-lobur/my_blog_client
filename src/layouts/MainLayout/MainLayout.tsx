fed5bd3cbec03057c38d993f443b886cb9cfe339
Лобурь Ярослав <lobur@rasse.ru>
2021-01-29 20:34:59 +0300

    рефакторит оформление и стили

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,23 +1,22 @@
 import React, { FunctionComponent } from 'react';
 import { Layout } from 'antd';
 import classes from './MainLayout.module.css'
-import MainSider from "layouts/MainLayout/components/MainSider/MainSider";
+import MainHeader from './components/MainHeader/MainHeader';
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 const MainLayout: FunctionComponent = ({ children }) => {
 
     return (
         <Layout className={classes.main_layout}>
-            <MainSider/>
 
-            <Layout>
-                <Header className={classes.header}/>
-                <Content className={classes.content}>
-                    {children}
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>"My Blog" Created by Ya-Lobur ©2021</Footer>
-            </Layout>
+            <MainHeader/>
+
+            <Content className={classes.content}>
+                {children}
+            </Content>
+
+            <Footer className={classes.footer}>"My Blog" Created by Ya-Lobur ©2021</Footer>
         </Layout>
     );
 }
---
+++ src/layouts/MainLayout/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from 'react';
+import { Col, Layout, Row } from "antd";
+import classes from './MainHeader.module.css'
+import { useRouteMatch } from "react-router";
+import { RouteTemplates } from "constants/routes";
+import { HomeFilled, HomeOutlined } from "@ant-design/icons";
+import { Link } from 'react-router-dom';
+
+const MainHeader: FunctionComponent = () => {
+
+    const { Header } = Layout;
+    const isMainPage = useRouteMatch({ path: RouteTemplates.ROOT, exact: true })
+
+
+    return (
+        <Header className={classes.main_header}>
+            <Row>
+                <Col>
+                    <Link to={RouteTemplates.ROOT} className={classes.home_link}>
+                        {isMainPage ? <HomeOutlined/> : <HomeFilled/>}
+                    </Link>
+                </Col>
+            </Row>
+        </Header>
+    );
+}
+
+export default MainHeader;
