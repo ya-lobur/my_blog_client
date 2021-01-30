aa0aa701c092638f28fe8db326546e6588493277
Лобурь Ярослав <lobur@rasse.ru>
2021-01-30 19:15:21 +0300

    404-я страница

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,9 +3,11 @@ import IndexPage from "pages/IndexPage/IndexPage";
 import PostDetail from "pages/post/PostDetail/PostDetail";
 import PostWizard from "pages/post/wizard/PostWizard/PostWizard";
 import BlogDetail from "pages/BlogDetail/BlogDetail";
+import NotFoundPage from "pages/NotFoundPage/NotFoundPage";
 
 export enum RouteTemplates {
     ROOT = '/',
+    NOT_FOUND = '/404/',
     BLOG_DETAIL = '/blog/:blogId/',
     POST_DETAIL = '/post/:postId/',
     POST_CREATE = '/post-wizard/',
@@ -34,7 +36,11 @@ const routes: IAppRoute[] = [
     {
         path: [RouteTemplates.POST_CREATE, RouteTemplates.POST_EDIT],
         component: PostWizard
-    }
+    },
+    {
+        path: RouteTemplates.NOT_FOUND,
+        component: NotFoundPage
+    },
 ]
 
 export default routes
